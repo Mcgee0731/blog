722aef265778aec20a9d82193b841c4037d7da26
mcgee0731 <mcgee0731@163.com>
2022-02-16 23:11:42 +0800

    tijiao

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,8 @@
 <template>
   <Layout>
     <!-- Page Header-->
-    <header class="masthead" :style="{backgroundImage: `url(${GRIDSOME_API_URL + general.cover.url})`}">
+    <!-- `url(${GRIDSOME_API_URL + general.cover.url})` -->
+    <header class="masthead" :style="{backgroundImage: url(general.cover.url)}">
         <div class="container position-relative px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-10">
